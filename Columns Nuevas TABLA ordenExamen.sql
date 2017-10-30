64e27b096fef7697b8468ca7bee41a7a5d8a227e
aldosc <aldosparki@gmail.com>
2017-10-29 19:22:09 -0500

    ADD COLUMS ORDENEXAMEN

---
+++ Columns Nuevas TABLA ordenExamen.sql
@@ -0,0 +1,5 @@
+ALTER TABLE ordenExamen ADD estatusSol int;
+ALTER TABLE ordenExamen ADD fechaSol datetime;    
+ALTER TABLE ordenExamen ADD observaSol Varchar(100);
+ALTER TABLE ordenExamen ADD idUsuarioSol int ;
+
