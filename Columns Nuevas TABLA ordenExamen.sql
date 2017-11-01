cf0418ef12dae9188da7b95ad5f2511a710e4e1f
jmuga <32976441+jmuga@users.noreply.github.com>
2017-11-01 02:26:55 -0500

    Update
    Se agregó idSecuen.
    Agregar dato por default para EstatusSol

--- Columns Nuevas TABLA ordenExamen.sql
+++ Columns Nuevas TABLA ordenExamen.sql
@@ -1,4 +1,4 @@
-ALTER TABLE ordenExamen ADD estatusSol int;
+ALTER TABLE ordenExamen ADD estatusSol int default 0;
 ALTER TABLE ordenExamen ADD fechaSol datetime;    
 ALTER TABLE ordenExamen ADD observaSol Varchar(100);
 ALTER TABLE ordenExamen ADD idUsuarioSol int ;
